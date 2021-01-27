cb8da3f320ead7b3373d8982505cba33083ad541
Renato Oliveira <renato.oliveira83@outlook.com>
2021-01-27 15:31:20 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,26 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import './core/assets/styles/custom.scss';
+import './App.scss'
+import React from 'react';
+import Navbar from './core/components/Navbar';
 
 const App = () => {
-    const [counter, setCounter] = useState(0);
-
-    useEffect(() => {
-        console.log('Componente iniciado')
-    })
 
     return (
-        <div className="container mt-5">
-            <button className="btn btn-primary mr-5" onClick={() => setCounter(counter + 1)}>
-                +
-            </button>
-            <span>
-                {counter}
-            </span>
-            <button className="btn btn-primary ml-5" onClick={() => setCounter(counter - 1)}>
-                -
-            </button>
-            {counter > 5 && <h1>O valor é maior que 5</h1>}
-        </div>
+        <Navbar />
     );
+    
 }
 
 export default App;
